edd9a767d969984d6366e4705bc07fb0328ae4c3
Sten Eschweiler <s.eschweiler@geonius.nl>
2023-01-11 21:27:42 +0100

    test

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ function renderPlaces(places) {
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-       model.setAttribute('rotation', '0 180 0');
-       model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '0.2 0.2 0.2');
 
        model.addEventListener('loaded', () => {
